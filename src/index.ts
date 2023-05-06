138357d7509f819f562207fbcffcdf2898a0b20d
João Pedro Pinheiro <jprnp18@gmail.com>
2023-05-05 21:12:39 -0300

    Exercicios enum

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-let myList = [1, 2, 3];
-myList = myList.map((e) => e + 1);
-console.log(myList);
+const fs = require('fs');
+
+console.log(practice([1, 2, 3], 1));
+console.log(practice([1, 2, 3], 3));
+
+function printLinesOfFile(): void {
+	let filePath = process.cwd();
+	filePath = filePath + '/lines.txt';
+	fs.readFileSync(filePath).toString().split('\n').forEach((line: string) => console.log(line));
+}
+
+function multiply(value: number | undefined): number | undefined {
+	if (!value) {
+		return undefined;
+	}
+	return value * 5;
+}
+
+function practice(nums: number[], index: number): number {
+	return (nums[index] ?? index) * 5;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,53 @@
 fn main() {
-    let foo: Vec<u32> = vec![1, 2, 3].iter().map(| x | { x + 1 }).collect();
-    println!("{:?}", foo);
+    println!("{:?}", practice(vec![1, 2, 3], 1));
+    println!("{:?}", practice(vec![1, 2, 3], 3));
+}
+
+/**
+* This function reads the lines.txt file in the root of the project and prints it.
+* Toying around with it is a good practice for Iterators in Rust.
+**/
+fn read_file_example() -> () {
+    if let Ok(file) = std::fs::read_to_string("lines.txt") {
+        file
+            .lines()
+            .enumerate()
+            .filter(| (idx, _) | idx % 2 == 0)
+            .skip(2)
+            .take(2)
+            .for_each(| (_, x) | println!("{}", x));
+    }
+}
+
+enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+fn print_color(color: Color) {
+    match color {
+        Color::Blue => println!("blue"),
+        Color::Red => println!("red"),
+        Color::Green => println!("green")
+    };
+}
+
+fn multiply(value: Option<usize>) -> Option<usize> {
+    // return value.unwrap_or(0) * 5;
+    // if let Some(x) = value {
+    //     return x * 5;
+    // }
+    // 0;
+
+    // return value.map(|x| x * 5);
+    // if let Some(x) = value {
+    //     return Some(x * 5);
+    // }
+    // return None;
+    return Some(value? * 5);
+}
+
+fn practice(nums: Vec<usize>, index: usize) -> usize {
+    return nums.get(index).unwrap_or(&index) * 5;
 }
